d79a3da588795ba5e06f34a8ca78e84c1dfe44e6
Titwin <titwin@live.fr>
2020-01-23 23:08:46 +0100

    First commit

---
+++ Assets/Scripts/Meteo.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteo : MonoBehaviour
+{
+    public Vector3 windBase;
+    public GameObject prefab;
+    public int size = 20;
+    public float noiseMagnitude = 0.0f;
+    public float t = 0.0f;
+    public float alpha1 = 20;
+    public float alpha2 = 5;
+    public float alpha3 = 0.7f;
+
+    private int harmonic = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefab.SetActive(false);
+        for(int i=-size; i< size + 1; i++)
+            for (int j = -size; j < size + 1; j++)
+            {
+                GameObject go = Instantiate(prefab);
+                go.SetActive(true);
+                go.transform.localPosition = new Vector3(4 * i, 0, 4 * j);
+                Transform tree = go.transform.Find("tree");
+                tree.localPosition = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+                float scale = Random.Range(0.5f, 1.3f);
+                tree.localScale = new Vector3(scale, scale, scale);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+    }
+
+    public Vector3 GetWind(Vector3 position)
+    {
+        return windBase * GetWave(position);
+    }
+
+    private float GetWave(Vector3 position)
+    {
+        float result = 0.0f;
+        for (int i = 0; i < harmonic; i++)
+        {
+            result += Mathf.Sin(alpha1 * Vector3.Dot(windBase, position) + alpha2 * t);
+        }
+        return result;
+    }
+}
---
+++ Assets/Scripts/WindAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindAnimation : MonoBehaviour
+{
+    public Meteo meteo;
+    private Dictionary<Transform, Quaternion> initialRotation = new Dictionary<Transform, Quaternion>();
+    public Vector3 wind;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Stack iterativePath = new Stack();
+        iterativePath.Push(this.transform);
+        while (iterativePath.Count != 0)
+        {
+            // pop and add childs to path
+            Transform bone = (Transform)iterativePath.Pop();
+            foreach (Transform child in bone)
+                iterativePath.Push(child);
+
+            // process current bone
+            initialRotation[bone] = bone.localRotation;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wind = meteo.GetWind(transform.position);
+
+        // move skeleton relatively to wind attributes
+        Stack iterativePath = new Stack();
+        iterativePath.Push(this.transform);
+        while(iterativePath.Count != 0)
+        {
+            // pop and add childs to path
+            Transform bone = (Transform) iterativePath.Pop();
+            foreach (Transform child in bone)
+                iterativePath.Push(child);
+
+            // process current bone
+            Vector3 axis = Vector3.Cross(wind, bone.localPosition);
+            Quaternion q = Quaternion.AngleAxis(wind.magnitude * transform.localScale.x * 0.01f, axis);
+            bone.localRotation = initialRotation[bone] * q;
+        }
+    }
+}
